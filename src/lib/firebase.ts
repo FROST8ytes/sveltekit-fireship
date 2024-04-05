256b13c4d675fbe1fa4a28e717f7f8784b98db2a
Ammar Farhan Mohamad Rizam <ammar_22006911@utp.edu.my>
2024-04-06 03:58:33 +0800

    Implement login with Google

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, type User } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { writable } from 'svelte/store';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyA_3hH93KQvC-4CkDQrM-HKHpGamVoG5o4',
@@ -17,3 +18,26 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+function userStore() {
+	let unsubscribe: () => void;
+
+	if (!auth || !globalThis.window) {
+		console.warn('Auth is not initialized or not in browser.');
+
+		const { subscribe } = writable<User | null>(null);
+		return { subscribe };
+	}
+
+	const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+		unsubscribe = onAuthStateChanged(auth, (user) => {
+			set(user);
+		});
+
+		return () => unsubscribe();
+	});
+
+	return { subscribe };
+}
+
+export const user = userStore();
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,27 @@
+<script lang="ts">
+	import { auth, user } from '$lib/firebase';
+	import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+	import 'iconify-icon';
+
+	async function signInWithGoogle() {
+		const provider = new GoogleAuthProvider();
+		const user = await signInWithPopup(auth, provider);
+		console.log(user);
+	}
+</script>
+
 <h2>LOGIN</h2>
+
+{#if $user}
+	<h2 class="card-title">Welcome, {$user.displayName}!</h2>
+	<p class="text-success text-center">You are logged in.</p>
+	<button class="btn btn-warning" on:click={() => signOut(auth)}>
+		<iconify-icon icon="uis:signout"></iconify-icon>
+		Sign Out
+	</button>
+{:else}
+	<button class="btn btn-primary" on:click={signInWithGoogle}>
+		<iconify-icon icon="bi:google"></iconify-icon>
+		Sign In with Google
+	</button>
+{/if}
